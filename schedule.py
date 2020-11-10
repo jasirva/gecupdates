df1515b81788055e1dbe476557ec5ad004c3a3e0
jasirva <67776707+jasirva@users.noreply.github.com>
2020-11-10 21:13:01 +0530

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -2,7 +2,7 @@ import schedule
 
 # bot and chat ids
 bot = 1164343404:AAFWtA6VxjsG6OUNLhEnO9GPpvvNLX-k0Rk
-chat_id = http://t.me/gecupdatesBot
+chat_id = gecupdatesBot
 
 # schedule crawler
 schedule.every().day.at("21:12").do(check_result_send_mess)
