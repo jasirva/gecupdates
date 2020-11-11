ad1e8ce75a93e77aaf72fa21b096d79c9037ed9b
jasirva <67776707+jasirva@users.noreply.github.com>
2020-11-11 18:30:59 +0530

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -2,11 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 def crawling(website_link, link_class):
-    '''
-    Args: website_link = string; link of website to be crawled
-          link_class = string; class name for job link on website
-    Returns: jobs_link = list; list of jobs 
-    '''
+  
     
     # get content of website and parse it
     website_request = requests.get('https://ktu.edu.in/eu/core/announcements.htm', timeout=5)
