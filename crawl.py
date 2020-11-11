d6b23053e22209665d5ffec0472f3970a1d556cd
jasirva <67776707+jasirva@users.noreply.github.com>
2020-11-11 14:51:06 +0530

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -9,9 +9,9 @@ def crawling(website_link, link_class):
     '''
     
     # get content of website and parse it
-    website_request = requests.get(website_link, timeout=5)
-    website_content = BeautifulSoup(website_request.content, ‘html.parser’)
+    website_request = requests.get('https://ktu.edu.in/eu/core/announcements.htm', timeout=5)
+    website_content = BeautifulSoup(website_request.content, 'html.parser')
     
     # extract job description
-    jobs_link = website_content.find_all(class_ = link_class)
+    jobs_link = website_content.find_all(class_ = 'b')
     return jobs_link
